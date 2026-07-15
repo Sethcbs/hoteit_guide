e01a6ff06fb4b2bdcbb3c79856478a8c030a16f0
sethcbs <sethmc22@gmail.com>
2026-07-15 12:01:47 -0700

    added GetCity function

--- cross-challenge/data.py
+++ cross-challenge/data.py
@@ -7,7 +7,7 @@ def GetNames():
     response = requests.get(
             "https://randommer.io/api/Name",
             params={"nametype": "firstname", "quantity": 100},
-            headers={"x-api-key": ""},
+            headers={"x-api-key": "e558414d940d4a18ad383ab22d502b82"},
             timeout = 10,
     )
     
@@ -15,6 +15,12 @@ def GetNames():
     names = response.json()
     return names
 
-
+#our api does not generate cities, so this funciton will return a random one
+def GetCity():
+    cities = ["Vancouver", "Concord", "Portland", "Seattle", "Beaverton", "Ridgefield", "Pleasant Hill", "Walnut Creek", "Tigard", "San Francisco"]
+    i = random.randint(1, 9)
+    return cities[i]
+    
+    
 if __name__ == "__main__":
-    print(GetNames())
+    print(GetCity())
